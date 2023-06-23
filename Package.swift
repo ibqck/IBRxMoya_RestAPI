ab046ad229515cb23c27d7c0467ebfc05ceb7d33
InBeomPyo <nankkang12@naver.com>
2023-06-23 09:31:01 +0900

    의존성 Framework 추가

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+
 import PackageDescription
 
 let package = Package(
@@ -10,6 +11,13 @@ let package = Package(
         .library(
             name: "IBRxMoya_RestAPI",
             targets: ["IBRxMoya_RestAPI"]),
+    ],
+    dependencies: [
+        //.package(url: "https://github.com/ibqck/IBRxMoya_RestAPI", from: "1.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
+        .package(url: "https://github.com/Moya/Moya.git", from: "15.0.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
